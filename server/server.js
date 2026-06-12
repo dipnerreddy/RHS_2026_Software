b478a769d8cca28ea775ec6547f67fb16055d961
dipnerreddy <dipnerreddy9@gmail.com>
2026-06-12 20:47:10 +0530

    Connected the Mango DB with the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -9,10 +15,12 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Backend is running successfully!"
+    success: true,
+    message: "Backend is running successfully",
   });
 });
-const PORT = 8090;
+
+const PORT = process.env.PORT || 8090;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
