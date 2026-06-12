fe9a24676e95e0595639ab57c5af452936485e44
dipnerreddy <dipnerreddy9@gmail.com>
2026-06-12 21:01:21 +0530

    User schema is updated and tested with the test data for the creation of user

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 
 dotenv.config();
 
@@ -12,7 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.json({
     success: true,
