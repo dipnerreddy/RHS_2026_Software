b51c2924cd6e77c7373009fc2b24b71cfc330c77
dipnerreddy <dipnerreddy9@gmail.com>
2026-06-12 22:41:30 +0530

    completed the core backend authentication layer.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -122,7 +122,23 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getProfile = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Profile fetched successfully",
+      user: req.user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error"
+    });
+  }
+};
+
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  getProfile
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const {
   registerUser,
-  loginUser
+  loginUser,
+  getProfile
 } = require("../controllers/authController");
+
+const authenticateUser = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
+
+router.get(
+  "/profile",
+  authenticateUser,
+  getProfile
+);
 
 module.exports = router;
